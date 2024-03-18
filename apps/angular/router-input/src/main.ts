f739fc61b5043db3e282c25ab735bbd7c1af0d02
Simon <karinn.71@gmail.com>
2024-03-18 16:02:19 +0200

    router-input

--- apps/angular/router-input/src/main.ts
+++ apps/angular/router-input/src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { appRoutes } from './app/app.routes';
+
+const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(appRoutes, withComponentInputBinding()),    
+  ],
+};
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
